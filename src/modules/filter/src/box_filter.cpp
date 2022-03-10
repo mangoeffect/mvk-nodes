54e787c19626d5819e07af720eb512ebf4cc178e
mango <2321544362@qq.com>
2022-03-10 20:03:45 +0800

    feat(mvk_filter) box-filter with border-padding

--- src/modules/filter/src/box_filter.cpp
+++ src/modules/filter/src/box_filter.cpp
@@ -17,8 +17,12 @@ namespace mvk
 {
     int BoxFilter(const Image& src, Image& dst, const size_t kernel_size, const BORDER_TYPE& border_type)
     {
-        //1. Todo: 边界扩充
-        auto src_padding = src;
+        size_t half_kernel = kernel_size / 2;
+        size_t border_size = half_kernel;
+        size_t window_size = 2 * half_kernel + 1;
+        size_t window_area = window_size * window_size;
+        //1. 边界扩充
+        auto src_padding = src.CopyWithBorder(border_size);
 
         //2. 计算积分图
         Matrix<uint64_t> integral;
@@ -29,32 +33,32 @@ namespace mvk
 
         //3. 计算滤波值
         dst = src.Copy();
-        size_t half_kernel = kernel_size / 2;
-        size_t window_size = 2 * half_kernel + 1;
-        size_t window_area = window_size * window_size;
 
         if(half_kernel > 0)
         {
             if(auto channels = src.GetChannel(); channels == 1)
             {
-                for(size_t i = half_kernel; i < dst.GetHeight() - half_kernel; i++) //row
+                for(size_t i = 0; i < dst.GetHeight(); i++) //row
                 {
-                    for(size_t j = half_kernel ; j < dst.GetWidth() - half_kernel; j++)//col
+                    for(size_t j = 0 ; j < dst.GetWidth(); j++)//col
                     {
                         //--------------
                         // |  A  |  B |
                         //   ----p ----
                         // |  C  |  D |
                         // Sum_abcd = Sat_d - Sat_b - Sat_c + Sat_a
-                        auto sum = integral(i + half_kernel, j + half_kernel) - integral(i - half_kernel, j + half_kernel) - integral(i + half_kernel, j - half_kernel) + integral(i - half_kernel, j - half_kernel);
+                        auto sum = integral(border_size + i + half_kernel, border_size + j + half_kernel) 
+                                 - integral(border_size + i - half_kernel, border_size + j + half_kernel) 
+                                 - integral(border_size + i + half_kernel, border_size + j - half_kernel) 
+                                 + integral(border_size + i - half_kernel, border_size + j - half_kernel);
                         dst.GetPixel(i, j)[0] = static_cast<uint8_t>(std::round(1.0 * sum / window_area));
                     }
                 }
             }else
             {
-                for(size_t i = half_kernel; i < dst.GetHeight() - half_kernel; i++) //row
+                for(size_t i = 0; i < dst.GetHeight(); i++) //row
                 {
-                    for(size_t j = half_kernel ; j < dst.GetWidth() - half_kernel; j++)//col
+                    for(size_t j = 0; j < dst.GetWidth(); j++)//col
                     {
                         for(size_t c = 0; c < channels; c++)
                         {
@@ -63,10 +67,10 @@ namespace mvk
                             //   ----p ----
                             // |  C  |  D |
                             // Sum_abcd = Sat_d - Sat_b - Sat_c + Sat_a
-                            auto sum = integral(i + half_kernel, (j + half_kernel) * channels + c) 
-                                     - integral(i - half_kernel, (j + half_kernel) * channels + c) 
-                                     - integral(i + half_kernel, (j - half_kernel) * channels + c) 
-                                     + integral(i - half_kernel, (j - half_kernel) * channels + c);
+                            auto sum = integral(border_size + i + half_kernel, (border_size + j + half_kernel) * channels + c) 
+                                     - integral(border_size + i - half_kernel, (border_size + j + half_kernel) * channels + c) 
+                                     - integral(border_size + i + half_kernel, (border_size + j - half_kernel) * channels + c) 
+                                     + integral(border_size + i - half_kernel, (border_size + j - half_kernel) * channels + c);
                             dst.GetPixel(i, j)[c] = static_cast<uint8_t>(std::round(1.0 * sum / window_area));
                         }
                     }
