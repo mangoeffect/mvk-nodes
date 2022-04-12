756cf7d74eef8a76f1be585f67efae5c18fe4715
mango <2321544362@qq.com>
2022-04-12 15:58:24 +0800

    feat(mvk_common) common export

--- src/modules/common/include/common/common_defines.hpp
+++ src/modules/common/include/common/common_defines.hpp
@@ -12,6 +12,9 @@
 #ifndef MVK_COMMON_DEFINES_HPP_
 #define MVK_COMMON_DEFINES_HPP_
 
+#ifndef BUILD_STATIC_LIB
+  #include "mvk_common_export.hpp"
+#endif 
 #if __cplusplus >= 202003L
 #include <numbers>
 #endif 
@@ -23,6 +26,16 @@ namespace mvk
     #else 
         #define MVK_PI 3.1415926
     #endif
+
+    /**
+     * @brief 分解矩阵方法
+     * 
+     */
+    enum class MATRIX_SOLVE_METHOD
+    {
+        LU = 0,
+        SVD = 1
+    };
 }//namespace mvk
 
 #endif 
