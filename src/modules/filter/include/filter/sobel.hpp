85bf0c9e456abaefe0892088f8c14f40e4184d96
mango <2321544362@qq.com>
2022-04-12 23:54:09 +0800

    feat(mvk_filter) Add canny and sobel

---
+++ src/modules/filter/include/filter/sobel.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file sobel.hpp
+ * @author mango (2321544362@qq.com)
+ * @brief 索贝尔算子实现
+ * @version 0.1
+ * @date 2022-04-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef MVK_FILTER_SOBEL_HPP_
+#define MVK_FILTER_SOBEL_HPP_
+
+namespace mvk
+{
+    
+}//namespace mvk
+
+#endif //MVK_FILTER_SOBEL_HPP_
